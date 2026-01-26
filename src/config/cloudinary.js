14b5ed1988ecc2f16b06253692b74b2321762f28
Manish Kumar <manishkumar.dev08@gmail.com>
2026-01-26 15:24:44 +0530

    fix config

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.db3jrilzf,
-  api_key: process.env.v,
-  api_secret: process.env.dfESW61QI8A1VHOgdudF7DuouzM,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
